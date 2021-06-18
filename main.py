6611156429d50e53227f82d1db34193c562832ec
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2021-06-18 04:24:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+from pyrogram import Client, filters, handlers
+import logging
+import os
+import requests
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+# TOKEN = os.getenv("TOKEN")
+TOKEN = "1280949531:AAH1jhZtuHBTXgDAztTYgvXMVOvJiYvnSJc"
+main_channel = -1001452061786
+CHANNELS = [-1001296393536, -1001208594988]
+
+app = Client(
+    "my_account",
+    api_id=1472621,
+    api_hash="188e8b4d277a61fe4bef4a7b6f2fa4bb",
+    bot_token=TOKEN
+)
+
+@app.on_message(filters.command(["start", "help"]) & filters.private)
+def start(client, message):
+    chat_id = message.from_user.id
+    app.send_message(chat_id, "**You Have No Permission to use this!**")
+
+@app.on_message(filters.text | filters.photo | filters.document)
+def main(client, message):
+    if message.chat.id == main_channel:
+        for channelID in CHANNELS:
+            app.copy_message(channelID, main_channel, message.message_id)
+
+app.add_handler(handlers.MessageHandler(start, filters.command(
+    ["start", "help"]) & filters.private))
+app.add_handler(handlers.MessageHandler(main, filters.text | filters.photo | filters.document))
+app.run()
