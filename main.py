16e3e2bc89127a8d2db9cb6c713c985d744221da
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2022-07-08 17:03:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def start(client, message):
 def main(client, message):
     if message.chat.id == main_channel:
             for channel in CHANNELS:
-                print(message)
             	app.copy_message(channel, main_channel, message.id)
 
 app.add_handler(handlers.MessageHandler(start, filters.command(
